667887972b719e0a739645682c4816ab4a389ca6
Graham Thomas <grahamthethomas@gmail.com>
2015-11-10 12:53:27 -0800

    Update EnemyHit.cs

--- EnemyHit.cs
+++ EnemyHit.cs
@@ -7,10 +7,12 @@ public class EnemyHit : MonoBehaviour {
 	//Damages the Player
 	void Update()
 	{
+		//Stop lasting hitboxes
 		timeSensitive++;
 	}
 	void OnTriggerEnter(Collider colid)
 	{
+		//If the player is in the hitbox of the punch. Lose health and play audio.
 		if (colid.tag == "Player" && timeSensitive < 3 && !colid.GetComponent<Mover>().dead) {
 			GetComponent<AudioSource>().Play ();
 			colid.GetComponent<Mover>().health -= this.GetComponentInParent<Enemy>().damage;
